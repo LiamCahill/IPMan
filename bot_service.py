51573f2824242940c1a224cb2ed68df4328ae1a5
Liam <42883622+LiamCahill@users.noreply.github.com>
2023-12-27 12:22:16 -0500

    Remove obsolete function.

--- bot_service.py
+++ bot_service.py
@@ -1,13 +1,6 @@
 import os
 import json
 
-def get_local_token():
-    path = "./token.json"
-    if os.path.exists(path):
-        with open(path,'r') as file:
-            KEY = json.load(file)
-        return KEY['DISCORD_TOKEN']
-
 
 def get_local_attribute(attr: str):
     path = "./token.json"
